3a69d0b436ef9169aadabbd5878b4a4009a16690
Julian Boyko <julian.boyko@outlook.com>
2021-06-21 12:29:12 -0400

    Implemented SQLite Persistant Storage

---
+++ Assignment2/Models/FoodDataDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+namespace Assignment2.Models
+{
+    public class FoodDataDB
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string food_name { get; set; }
+        public string nix_brand_id { get; set; }
+        public Double nf_calories { get; set; }
+        public string photoUrl { get; set; }
+        public string brand_name { get; set; }
+        public string nix_item_id { get; set; }
+    }
+}
